d2336c51ece32401d37d038d6a6bdc077efcd219
iuliana <iuliana.cosmina@gmail.com>
2019-04-28 00:06:42 +0100

    20 started boot

---
+++ chapter04/boot-aop/src/main/java/com/apress/cems/boot/entities/Person.java
@@ -0,0 +1,109 @@
+/*
+Freeware License, some rights reserved
+
+Copyright (c) 2019 Iuliana Cosmina
+
+Permission is hereby granted, free of charge, to anyone obtaining a copy 
+of this software and associated documentation files (the "Software"), 
+to work with the Software within the limits of freeware distribution and fair use. 
+This includes the rights to use, copy, and modify the Software for personal use. 
+Users are also allowed and encouraged to submit corrections and modifications 
+to the Software for the benefit of other users.
+
+It is not allowed to reuse,  modify, or redistribute the Software for 
+commercial use in any way, or for a user's educational materials such as books 
+or blog articles without prior permission from the copyright holder. 
+
+The above copyright notice and this permission notice need to be included 
+in all copies or substantial portions of the software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS OR APRESS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package com.apress.cems.boot.entities;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+/**
+ * @author Iuliana Cosmina
+ * @since 1.0
+ */
+@Entity
+@NamedQueries({
+        @NamedQuery(name = Person.FIND_ALL, query = "select p from Person p"),
+        @NamedQuery(name = Person.FIND_BY_ID, query = "select p from Person p where p.id=:id"),
+        @NamedQuery(name = Person.COUNT_ALL, query = "select count(p) from Person p")
+})
+public class Person {
+
+    public static final String FIND_ALL = "Person.findAll";
+    public static final String FIND_BY_ID = "Person.findById";
+    public static final String COUNT_ALL = "Person.countAll";
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(updatable = false)
+    protected Long id;
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Person person = (Person) o;
+        return Objects.equals(firstName, person.getFirstName()) &&
+                Objects.equals(lastName, person.getLastName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person[username='%s%n', firstName='%s', lastName='%s']"
+                , firstName, lastName);
+
+    }
+}
---
+++ chapter04/boot-aop/src/main/java/com/apress/cems/boot/services/PersonServiceImpl.java
@@ -0,0 +1,81 @@
+/*
+Freeware License, some rights reserved
+
+Copyright (c) 2019 Iuliana Cosmina
+
+Permission is hereby granted, free of charge, to anyone obtaining a copy 
+of this software and associated documentation files (the "Software"), 
+to work with the Software within the limits of freeware distribution and fair use. 
+This includes the rights to use, copy, and modify the Software for personal use. 
+Users are also allowed and encouraged to submit corrections and modifications 
+to the Software for the benefit of other users.
+
+It is not allowed to reuse,  modify, or redistribute the Software for 
+commercial use in any way, or for a user's educational materials such as books 
+or blog articles without prior permission from the copyright holder. 
+
+The above copyright notice and this permission notice need to be included 
+in all copies or substantial portions of the software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS OR APRESS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package com.apress.cems.boot.services;
+
+import com.apress.cems.boot.entities.Person;
+import com.apress.cems.boot.repos.PersonRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * @author Iuliana Cosmina
+ * @since 1.0
+ */
+@Service
+@Transactional
+public class PersonServiceImpl implements PersonService {
+    private PersonRepo personRepo;
+
+    @Autowired
+    public PersonServiceImpl(PersonRepo personRepo) {
+        this.personRepo = personRepo;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Set<Person> findAll() {
+        Set<Person> persons = new HashSet<>();
+        personRepo.findAll().forEach(persons::add);
+        return persons;
+    }
+
+    @Override
+    public long count() {
+        return personRepo.count();
+    }
+
+    @Override
+    public Optional<Person> findById(Long id) {
+        return personRepo.findById(id);
+    }
+
+    @Override
+    public Person save(Person person) {
+        return personRepo.save(person);
+    }
+
+    @Override
+    public void delete(Person person) {
+        personRepo.delete(person);
+    }
+}
