9103329523ae28dd4c07eec8840d65a761928fb3
iuliana <iuliana.cosmina@gmail.com>
2019-05-29 00:25:02 +0100

    23 added more tests, more annotations, corrected a few

---
+++ chapter03/spring-profiles/src/test/java/com/apress/cems/profiles/SampleProfileConfigTest.java
@@ -0,0 +1,100 @@
+/*
+Freeware License, some rights reserved
+
+Copyright (c) 2019 Iuliana Cosmina
+
+Permission is hereby granted, free of charge, to anyone obtaining a copy 
+of this software and associated documentation files (the "Software"), 
+to work with the Software within the limits of freeware distribution and fair use. 
+This includes the rights to use, copy, and modify the Software for personal use. 
+Users are also allowed and encouraged to submit corrections and modifications 
+to the Software for the benefit of other users.
+
+It is not allowed to reuse,  modify, or redistribute the Software for 
+commercial use in any way, or for a user's educational materials such as books 
+or blog articles without prior permission from the copyright holder. 
+
+The above copyright notice and this permission notice need to be included 
+in all copies or substantial portions of the software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS OR APRESS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package com.apress.cems.profiles;
+
+import com.apress.cems.repos.DetectiveRepo;
+import com.apress.cems.repos.PersonRepo;
+import com.apress.cems.repos.impl.JdbcDetectiveRepo;
+import com.apress.cems.repos.impl.JdbcPersonRepo;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+
+/**
+ * @author Iuliana Cosmina
+ * @since 1.0
+ */
+@SpringJUnitConfig(classes = SampleProfileConfig.TestCtxConfig.class)
+class SampleProfileConfig {
+
+    private Logger logger = LoggerFactory.getLogger(SimpleBeanImpl.class);
+
+    @Autowired
+    ApplicationContext ctx;
+
+    @Test
+    void testBean(){
+        Arrays.stream(ctx.getBeanDefinitionNames()).forEach();
+    }
+
+
+    @Configuration
+    static class TestCtxConfig {
+
+        @Bean
+        @Profile("one")
+        PersonRepo jdbcPersonRepo() {
+            return new JdbcPersonRepo(jdbcTemplate());
+        }
+
+        @Before
+        @Profile("two")
+        DetectiveRepo jdbcDetectiveRepo(){
+            return new JdbcDetectiveRepo(jdbcTemplate());
+        }
+
+        @Bean
+        JdbcTemplate jdbcTemplate() {
+            return new JdbcTemplate(dataSource());
+        }
+
+        @Bean
+        public DataSource dataSource() {
+            EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+            EmbeddedDatabase db = builder
+                    .setType(EmbeddedDatabaseType.H2)
+                    .generateUniqueName(true)
+                    .build();
+            return db;
+        }
+    }
+}
---
+++ chapter03/spring-tests-jupiter/src/test/java/com/apress/cems/testrepos/RepositoryTest6.java
@@ -0,0 +1,114 @@
+/*
+Freeware License, some rights reserved
+
+Copyright (c) 2019 Iuliana Cosmina
+
+Permission is hereby granted, free of charge, to anyone obtaining a copy 
+of this software and associated documentation files (the "Software"), 
+to work with the Software within the limits of freeware distribution and fair use. 
+This includes the rights to use, copy, and modify the Software for personal use. 
+Users are also allowed and encouraged to submit corrections and modifications 
+to the Software for the benefit of other users.
+
+It is not allowed to reuse,  modify, or redistribute the Software for 
+commercial use in any way, or for a user's educational materials such as books 
+or blog articles without prior permission from the copyright holder. 
+
+The above copyright notice and this permission notice need to be included 
+in all copies or substantial portions of the software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS OR APRESS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package com.apress.cems.testrepos;
+
+import com.apress.cems.dao.Person;
+import com.apress.cems.repos.PersonRepo;
+import com.apress.cems.repos.impl.JdbcPersonRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import javax.sql.DataSource;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Iuliana Cosmina
+ * @since 1.0
+ */
+@Sql(
+        scripts = "classpath:db/test-data.sql",
+        config = @SqlConfig(commentPrefix = "`", separator = "@@")
+)
+@Sql(statements = "DELETE FROM PERSON", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@SpringJUnitConfig(classes = RepositoryTest5.TestCtxConfig.class)
+public class RepositoryTest5 {
+    static final Long PERSON_ID = 1L;
+
+    @Autowired
+    PersonRepo personRepo;
+
+    @BeforeEach
+    void setUp() {
+        assertNotNull(personRepo);
+    }
+
+    @Test
+    void testFindByIdPositive() {
+        Person person = personRepo.findById(PERSON_ID);
+        assertNotNull(person);
+        assertEquals("Sherlock", person.getFirstName());
+    }
+
+    @Test
+    void testFindAll(){
+        Set<Person> all = personRepo.findAll();
+        assertAll(
+                () -> assertNotNull(all),
+                () -> assertEquals(2, all.size())
+        );
+    }
+
+    @Configuration
+    static class TestCtxConfig {
+        @Bean
+        PersonRepo jdbcPersonRepo() {
+            return new JdbcPersonRepo(jdbcTemplate());
+        }
+
+        @Bean
+        JdbcTemplate jdbcTemplate() {
+            return new JdbcTemplate(dataSource());
+        }
+
+        @Bean
+        public DataSource dataSource() {
+            EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+            EmbeddedDatabase db = builder
+                    .setType(EmbeddedDatabaseType.H2)
+                    .generateUniqueName(true)
+                    .addScript("db/schema.sql")
+                    .build();
+            return db;
+        }
+    }
+
+}
+
