81b517747bbe822fd9cba0b3549daccdd383d55d
iuliana <iuliana.cosmina@gmail.com>
2020-04-12 16:52:58 +0100

    added example for self invocating hoax

---
+++ chapter04/aop-extras/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/**
+ * Created by iuliana.cosmina on 21/01/19.
+ */
+module com.apress.cems.aop.extras {
+    requires com.apress.cems.dao;
+    requires com.apress.cems.repos;
+    requires org.apache.commons.lang3;
+    requires java.sql;
+    requires spring.jdbc;
+
+    requires spring.context;
+    requires spring.beans;
+    requires spring.core;
+    requires spring.aop;
+    requires org.aspectj.weaver;
+    requires org.slf4j;
+}
